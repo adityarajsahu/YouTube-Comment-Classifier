2f0515e2a6ce122f1a377318252f56cb2484ba42
adityarajsahu <adityaraj2019.sahu@gmail.com>
2020-10-30 22:18:36 +0530

    Pre-processing the dataset before training

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,7 +1,6 @@
 #!usr/bin/env python
 
-from sklearn.feature_extraction.text import CountVectorizer
-import os
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import pickle
 
 
@@ -12,18 +11,25 @@ def processor(df):
     df_content = df['CONTENT']
     df_label = df['CLASS']
     
-    vectorizer = CountVectorizer(ngram_range=(0, 2), lowercase=True, decode_error="replace")
-    df_token = vectorizer.transform(df_content)
-    os.chdir("SavedModel/")
-    filename = 'vectorizer1.pk'
-    pickle.dump(vectorizer, open(filename, 'wb'))
+    vectorizer = CountVectorizer(ngram_range=(0, 2), lowercase=True)
+    df_vectorized = vectorizer.fit_transform(df_content)
     
-    df_vector_train = df_token[:1700]
-    df_vector_val = df_token[1700:]
+    filename_vec = 'SavedModel/vectorizer1.pkl'
+    pickle.dump(vectorizer, open(filename_vec, 'wb'))
     
-    df_label_train = df_label[:1700]
-    df_label_val = df_label[1700:]
+    transformer = TfidfTransformer()
+    df_transformed = transformer.fit_transform(df_vectorized)
+    filename_trans = 'SavedModel/transformer1.pkl'
+    pickle.dump(transformer, open(filename_trans, 'wb'))
     
-    content = [df_vector_train, df_vector_val]
-    label = [df_label_train, df_label_val]
+    dfContent_train = df_transformed[:1700]
+    dfContent_val = df_transformed[1700:1900]
+    dfContent_test = df_transformed[1900:]
+    
+    dfLabel_train = df_label[:1700]
+    dfLabel_val = df_label[1700:1900]
+    dfLabel_test = df_label[1900:]
+    
+    content = [dfContent_train, dfContent_val, dfContent_test]
+    label = [dfLabel_train, dfLabel_val, dfLabel_test]
     return content, label
