f5421716c042a01a2a714728524170472d5360eb
adityarajsahu <adityaraj2019.sahu@gmail.com>
2020-10-30 22:17:35 +0530

    Code for predicting comments

--- classifier.py
+++ classifier.py
@@ -1,21 +1,27 @@
 #!usr/bin/env python
 
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
 import pickle
-import os
+import pandas as pd
 
-comment = [input("Enter the YouTube comment to be verified : ")]
+testdf = pd.read_csv('Dataset/testData.csv')
+testContent = testdf['CONTENT']
+testLabel = testdf['CLASS']
 
-os.chdir("SavedModel/")
-loaded_vocab = pickle.load(open('vectorizer1.pk', 'rb'))
-loaded_vectorizer = CountVectorizer(vocabulary=loaded_vocab)
-vectorized_comment = loaded_vectorizer.fit_transform(np.array(comment))
+loaded_vectorizer = pickle.load(open('SavedModel/vectorizer1.pkl', 'rb'))
+vectorized_content = loaded_vectorizer.transform(testContent)
 
-loaded_model = pickle.load(open('training1_model.sav', 'rb'))
-prediction_score = loaded_model.predict(vectorized_comment)
+loaded_transformer = pickle.load(open('SavedModel/transformer1.pkl', 'rb'))
+transformed_content = loaded_transformer.transform(vectorized_content)
 
-if prediction_score < 0.5:
-    print("It is not a spam comment.")
-else:
-    print("It's a spam comment. Please delete it.")
+loaded_model = pickle.load(open('SavedModel/training1_model.pkl', 'rb'))
+predictions = loaded_model.predict(transformed_content)
+
+correct_predictions = 0
+for i in range(len(testLabel)):
+    if predictions[i] == testLabel[i]:
+        correct_predictions = correct_predictions + 1
+    else:
+        pass
+
+testAccuracy = correct_predictions/len(predictions)
+print("Accuracy on Test Dataset : {:.3f}%".format(testAccuracy * 100))
